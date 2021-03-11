80c35ce6004d8ce3440f0877d5821d3b6c108ba1
Aaron Fay <aaron.j.fay@gmail.com>
2021-03-10 18:45:53 -0800

    swap out python for jq in test script

--- scripts/test-plugin.sh
+++ scripts/test-plugin.sh
@@ -6,6 +6,10 @@ cd /tmp
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 chmod 744 jenkins-cli.jar
 
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+mv ./jq-linux64 ./jq
+chmod +x ./jq
+
 # Trigger the job
 echo "Triggering build for $JOB"
 java -jar jenkins-cli.jar -s http://localhost:8080/ build $JOB
@@ -14,7 +18,7 @@ STATUS='None'
 echo "Polling for job result"
 while [ "$STATUS" = 'None' ]; do
   sleep 5
-  STATUS=$(curl -s "http://localhost:8080/job/$JOB/lastBuild/api/json" | python -c 'import json; import sys; data=json.load(sys.stdin); print data.get("result")')
+  STATUS=$(curl -s "http://localhost:8080/job/$JOB/lastBuild/api/json" | ./jq '.result')
   echo " - status: $STATUS"
 done
 
